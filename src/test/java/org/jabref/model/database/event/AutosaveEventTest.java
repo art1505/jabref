dacace37887f2167943386bd6e201abf32315cb0
art1505 <78550466+art1505@users.noreply.github.com>
2021-10-24 20:27:29 -0300

    addingTestWhenIsNull

--- src/test/java/org/jabref/model/database/event/AutosaveEventTest.java
+++ src/test/java/org/jabref/model/database/event/AutosaveEventTest.java
@@ -6,9 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AutosaveEventTest {
 
+    private Object Null;
+    
     @Test
     public void givenNothingWhenCreatingThenNotNull() {
         AutosaveEvent e = new AutosaveEvent();
         assertNotNull(e);
+    }
+    
+    @Test
+    public void givenNULLWhenCreatingThenNull() {
+        AutosaveEvent Null = new AutosaveEvent();
+        assertNotNull(Null);
     }
 }
